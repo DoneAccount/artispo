34e3f2d314bc99a1ea95cfe4acdeda082752db17
DoneAccount <96767782+DoneAccount@users.noreply.github.com>
2026-01-26 16:55:05 +0800

    Image upload files

---
+++ image_upload.php
@@ -0,0 +1,14 @@
+<?php
+
+    /* For Jin:
+    DELETE THIS BEFORE SUBMISSION
+    tatlo yung files na kailangan gawin pwede kang
+    mag assign ng task sa kanila pero pasupervise nalang 8)
+    
+    gagawin niyo is image upload na may upload date parang
+    yung sa original na website niyo 8)
+
+    tanong ka lang sakin pag nalilito ka 8)
+    */
+
+?>
